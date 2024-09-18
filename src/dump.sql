1463a944f26c61e8eb5516d95c5e785d426df98e
Anderson583 <andersonquirino@outlook.com>
2024-09-18 19:16:50 -0300

    parte 10

--- src/dump.sql
+++ src/dump.sql
@@ -5,7 +5,7 @@ create database anderson;
 create table if not exists users(
     id serial primary key,
     name varchar(255),
-    email varchar(255),
+    email varchar(255) unique,
     password varchar(255)
 )
 
