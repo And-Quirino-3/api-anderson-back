7420e4760d6ad0cad7d8571b0aff37e101fe4746
Anderson583 <andersonquirino@outlook.com>
2024-09-17 21:53:04 -0300

    parte 03

---
+++ src/dados/dados.js
@@ -0,0 +1,28 @@
+const users = [
+    {
+        id: 1,
+        name: "Dinho",
+        email: "dinho@email.com",
+        password: "htY584"
+    },
+    {
+        id: 2,
+        name: "Maria",
+        email: "maria@email.com",
+        password: "6Ytj7"
+    },
+    {
+        id: 3,
+        name: "Lia",
+        email: "lia@email.com",
+        password: "jUt5R"
+    },
+    {
+        id: 4,
+        name: "Fernanda",
+        email: "fernanda@email.com",
+        password: "54Gtm"
+    }
+]
+
+module.exports = users
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const users = require('./dados/dados')
 
 const app = express()
 
-app.get('/login', (req, res) => {
-    console.log("Dentro do Endpoint")
-    res.status(200).send("Server Dinho")
+app.get('/users', (req, res) => {
+    
+    res.status(200).send(users)
+})
+
+app.get('/users/:id', (req, res) => {
+    const {id} = req.params
+
+    const user = users.find( user => user.id === Number(id))
+    
+    res.status(200).send(user)
 })
 
 app.listen(3000, () => {
