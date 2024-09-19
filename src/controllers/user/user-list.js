6362e6bd608279de435cb98344fe5620ff0162fc
Anderson583 <andersonquirino@outlook.com>
2024-09-19 19:12:13 -0300

    parte 13

--- src/controllers/user/user-list.js
+++ src/controllers/user/user-list.js
@@ -3,7 +3,15 @@ const pool = require('../../connection')
 const usersList = async (_, res) => {
     try {
         const usersList = await pool.query('select * from users')
-        return res.status(200).send(usersList.rows)
+
+        const { rows } = usersList
+
+        const users = rows.map((user) => {
+            const { password: _, ...userData } = user
+            return userData
+        })
+
+        return res.status(200).send(users)
     } catch (error) {
         return res.status(500).send({ message: error.message })
     }
